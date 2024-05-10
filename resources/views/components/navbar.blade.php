c95a33c43c2d202c26259beadaf54ad5e5197df9
Kristaps <ipb22.k.liepins@vtdt.edu.lv>
2024-05-10 07:31:54 +0300

    I want bed, NOT CLOSE TO FINISH :(

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,15 +3,23 @@
         <div class="nav-links"> 
             <x-nav-link href="/">Home</x-nav-link>
             <x-nav-link href="/create">Add recipe</x-nav-link>
-            <x-nav-link href="login">Login</x-nav-link>
         </div>
-        <div>
-            <button id="theme-toggle" onclick="toggleMode()">Toggle dark mode</button>
+        <div class="button-div">
+            @if(!Auth::user())
+            <form action="/login">
+                <button class="auth-but">Login</button>
+            </form>
+            <form action="/register">
+                <button class="auth-but">Register</button>
+            </form>
+            @endif
+            <button id="theme-toggle" onclick="toggleMode()"></button>
             @auth
                 <div class="dropdown">
                     <button class="dropbtn">{{ Auth::user()->name }}</button>
                     <div class="dropdown-content">
                         <a href="/profile">Profile</a>
+                        <a href="/bookmarks">Bookmarks</a>
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <x-dropdown-link :href="route('logout')"
