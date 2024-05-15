ce7bae35cfafd8f6f6d23e6127c1221b25956982
Kristaps <ipb22.k.liepins@vtdt.edu.lv>
2024-05-16 00:40:56 +0300

    almost done bookmark kinda done

--- resources/views/bookmark/index.blade.php
+++ resources/views/bookmark/index.blade.php
@@ -1,14 +1,27 @@
 
-    <h1>Your Bookmarks</h1>
-
+<x-layout title="home">
+    <header class="bg-white shadow"><h1>Your Bookmarks</h1></header>
+    <div class="main" >
     @foreach ($bookmarks as $bookmark)
-        <div>
-            <h2>{{ $bookmark->food->name }}</h2>
-            <p>{{ $bookmark->food->description }}</p>
-            <form action="/foods/{{ $bookmark->food->id }}/bookmarks" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Remove Bookmark</button>
-            </form>
+        <div class="recipe">
+            <h2><a href="show/{{$bookmark->food->id}}">{{$bookmark->food->name}}</a></h2>
+            <div class="img"><img alt="img" src="{{$bookmark->food->image}}"></div>
+            <p>{{$bookmark->food->short_description}}</p>
+            <p>Rating: {{ ($bookmark->food->ratings != null && $bookmark->food->ratings->count() > 0) ? round($bookmark->food->ratings->avg('rating'),1) : 'No ratings yet' }}</p>
+            @auth
+                @if(Auth::user()->hasBookmarked($bookmark->food->id))
+                    <form action="/foods/{{ $bookmark->food->id }}/bookmarks" method="POST" class="form-book">
+                        @csrf
+                        @method('DELETE')
+                        <button class="bookmark" type="submit">Remove Bookmark</button>
+                    </form>
+                @else
+                    <form action="/foods/{{ $bookmark->food->id }}/bookmarks" method="POST" class="form-book">
+                        @csrf
+                        <button class="bookmark" type="submit">Bookmark</button>
+                    </form>
+                @endif
+            @endauth
         </div>
     @endforeach
+</x-layout>
